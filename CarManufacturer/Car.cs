d73d1070c15e9863cae0841aaf0d40efa6c547ae
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-11-26 12:18:43 +0200

    Add project files.

---
+++ CarManufacturer/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire [] Tires {get; set; }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - this.FuelConsumption * distance > 0)
+            {
+                this.FuelQuantity -= this.FuelConsumption * distance;
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoIAm()
+        {
+            return $"Make: {this.Make}\n" +
+                 $"Model: {this.Model}\n" +
+                 $"Year: {this.Year}\n" +
+                 $"Fuel: {this.FuelQuantity:F2}";
+        }
+        
+        public Car ()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.FuelConsumption = fuelConsumption;
+            this.FuelQuantity = fuelQuantity;
+
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+    }
+}
---
+++ CarManufacturer/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //string make=Console.ReadLine();
+            //string model=Console.ReadLine();
+            //int year=int.Parse(Console.ReadLine());
+            //double fuelConsumption=double.Parse(Console.ReadLine());
+            //double fuelQuantity=double.Parse(Console.ReadLine());
+
+            //Car firstcar = new Car();
+            //Car secondCar = new Car(make, model, year);
+            //Car thirdCar=new Car(make,model, year,fuelQuantity,fuelConsumption); 
+
+            var tires = new Tire[4]
+            {
+                new Tire(1,2.5),
+                new Tire(1,2.1),
+                new Tire(2,0.5),
+                new Tire(2,2.3),
+            };
+
+            Engine engine = new Engine(560, 6300);
+
+            Car car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+
+            
+        }
+    }
+}
